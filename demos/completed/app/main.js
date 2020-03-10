78f6f7c707b8059613fe565899292fc38e6439c0
JC Franco <jfranco@esri.com>
2020-03-10 10:14:19 -0700

    Use proxy only on localhost.

--- demos/completed/app/main.js
+++ demos/completed/app/main.js
@@ -5,14 +5,17 @@ define(["require", "exports", "esri/core/urlUtils", "./ItemScoreImprover"], func
     //  item setup
     //----------------
     var itemId = "696e66d1c1e54b3ea775fc93adb9c4e0";
-    urlUtils_1.addProxyRule({
-        urlPrefix: "https://www.arcgis.com",
-        proxyUrl: "http://localhost/proxy"
-    });
-    urlUtils_1.addProxyRule({
-        urlPrefix: "https://cdn.arcgis.com",
-        proxyUrl: "http://localhost/proxy"
-    });
+    // use resource proxy if we're in 'demo mode'
+    if (location.host === "localhost") {
+        urlUtils_1.addProxyRule({
+            urlPrefix: "https://www.arcgis.com",
+            proxyUrl: "http://localhost/proxy"
+        });
+        urlUtils_1.addProxyRule({
+            urlPrefix: "https://cdn.arcgis.com",
+            proxyUrl: "http://localhost/proxy"
+        });
+    }
     //----------------
     //  widget setup
     //----------------
--- demos/completed/app/main.ts
+++ demos/completed/app/main.ts
@@ -8,15 +8,18 @@ import ItemScoreImprover = require("./ItemScoreImprover");
 
 const itemId = "696e66d1c1e54b3ea775fc93adb9c4e0";
 
-addProxyRule({
-  urlPrefix: "https://www.arcgis.com",
-  proxyUrl: "http://localhost/proxy"
-});
+// use resource proxy if we're in 'demo mode'
+if (location.host === "localhost") {
+  addProxyRule({
+    urlPrefix: "https://www.arcgis.com",
+    proxyUrl: "http://localhost/proxy"
+  });
 
-addProxyRule({
-  urlPrefix: "https://cdn.arcgis.com",
-  proxyUrl: "http://localhost/proxy"
-});
+  addProxyRule({
+    urlPrefix: "https://cdn.arcgis.com",
+    proxyUrl: "http://localhost/proxy"
+  });
+}
 
 //----------------
 //  widget setup
