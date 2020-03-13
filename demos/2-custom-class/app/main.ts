5f80d63a67b348b48b946af746144f7c10acc242
Matt Driscoll <MDriscoll@esri.com>
2020-03-13 14:19:15 -0700

    comments

--- demos/2-custom-class/app/main.ts
+++ demos/2-custom-class/app/main.ts
@@ -9,3 +9,9 @@ const itemScore = new ItemScore();
 
 // add to window for demo
 (window as any).itemScore = itemScore;
+
+// itemScore.load();
+
+// itemScore.itemId = "f5a89635bb394f7da2f9c82cdd73e459";
+
+// itemScore.load();
